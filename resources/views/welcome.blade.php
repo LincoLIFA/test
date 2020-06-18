b6115ba49f776be63a188b8c4399300fa5122875
lincolifa <linco.lifa@gmail.com>
2020-06-18 01:05:25 -0400

    test v3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Laravel
+                github.com/lincolifa
             </div>
 
             <div class="links">
